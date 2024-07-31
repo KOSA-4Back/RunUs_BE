0236b3baf28995211c9f557926ba8b4c46650e88
DESKTOP-4U4G4G2\KOSA <dmswjd4566@hanmail.net>
2024-07-31 12:21:44 +0900

    Feat: 유저 프로필 조회 기능 구현
    - 유저 프로필 조회 기능 구현

--- src/main/java/com/fourback/runus/global/security/config/CorsConfig.java
+++ src/main/java/com/fourback/runus/global/security/config/CorsConfig.java
@@ -18,6 +18,8 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
  * DATE              AUTHOR             NOTE
  * -----------------------------------------------------------
  * 2024-07-24        김은정            최초 생성
+ * 2024-07-29        김민정            cors 프론트 포트 설정
+ * 2024-07-30        김은정            cors ngrock 주소 설정
  */
 @Configuration
 public class CorsConfig {
@@ -29,6 +31,7 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.addAllowedOriginPattern("http://localhost:3000"); //TODO 나중에 수정해야함
+        configuration.addAllowedOriginPattern("https://4b2d-115-93-148-232.ngrok-free.app");
         configuration.addAllowedMethod("*");
         configuration.addAllowedHeader("*");
         configuration.setAllowCredentials(true);
